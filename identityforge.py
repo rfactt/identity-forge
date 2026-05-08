78f6b39fc89a4454e7e53144ad3e4cc6b23e19b4
Rafael <rafaeldebarrosgama@gmail.com>
2026-05-08 12:10:10 -0300

    refactor: modulariza arquiterura do Identify Forge

--- identityforge.py
+++ identityforge.py
@@ -1,133 +1,228 @@
-import os
 import random as rd
 
 
-def gerar_sugestoes(nome_usuario, tema_usuario, estilo, quantidade):
-    sugestoes = gerar_sugestoes(nome_usuario, tema_usuario, estilo, quantidade)
 
-    return sugestoes
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("=" * 30)
-    print("     IDENTIFY FORGE")
-    print("=" * 30)
+def main():
+    nome = obter_nome()
 
-    while True:
-        
-        nome_usuario = input("Digite o nome do usuário: ").strip()
+    tema = obter_tema()
+    
+    estilo = obter_estilo()
 
-        if nome_usuario:
-            break
-        else:
-            print("O nome do usuário não pode estar vazio. Por favor, tente novamente.")
-    while True:        
-        tema_usuario = input("Digite um tema para sua identidade: ").strip()
-        if tema_usuario:
-            break
+    quantidade = obter_quantidade()
 
-        else:
-            print("O tema para a identidade não pode estar vazio. Por favor, tente novamente.")
+    sugestoes = gerar_sugestoes(
+        nome,
+        tema,
+        estilo,
+        quantidade
+    )
+
+    exibir_sugestoes(sugestoes)
+
+
+
+def obter_nome():
 
     while True:
-        print("\nEstilos Disponíveis:")
+
+        nome = input("Digite um nome de usuario:").strip()
+
+        if nome:
+            return nome
+        print("O nome nao pode estar vazio")
+
+
+def obter_tema():
+
+    while True:
+
+        tema = input("Escolha um tema:").strip().lower()
+
+        if tema:
+            return tema
+        print("O tema não pode estar vazio")
+
+
+def obter_estilo():
+
+    estilos_validos = [
+        "hacker",
+        "clean",
+        "profissional",
+        "criativo"
+    ]
+
+    while True:
+        print("\nEstilos Disponiveis")
         print("- Hacker")
         print("- Clean")
         print("- Profissional")
         print("- Criativo")
-        estilo = input("Escolha um estilo para sua identidade: ").lower().strip()
-        if estilo in ["hacker", "clean", "profissional", "criativo"]:
-            break
-        else:
-            print("Estilo inválido. Por favor, escolha um dos estilos disponíveis.")
 
-    nome_usuario =nome_usuario.lower().strip()
+        estilo = input("Escolha um estilo:").strip().lower()
+        if estilo in estilos_validos:
+            return estilo
+        
+        print("Estilo inválido.")
 
-    tema_usuario = tema_usuario.lower().strip()
-
-    estilo = estilo.lower().strip()
+def obter_quantidade():
 
     while True:
+
         try:
-            quantidade = int(input("Quantas sugestões de identidade você deseja? "))
-            if quantidade <= 0:
-                print("Por favor, insira um número positivo.")
-                continue
-            break
-        
+
+            quantidade = int(
+                input("Quantas sugestões deseja criar?:")
+                )
+            if quantidade > 0:
+                return quantidade
+
+            print("Digite um numero positivo")
+
         except ValueError:
-            print("Entrada inválida. Por favor, insira um número inteiro.")
+            print("Digite apenas numeros inteiros.")
 
 
-    if estilo == "hacker":
-        prefixos = ["Dark", "Sys", "root", "Cyber", "Shadow", "null"]
-        sufixos = ["fx", "core", "404", "byte", "exe", "link", "byte"]
-        separadores = ["_", ".", "-", ""]
+def gerar_sugestoes(
+    nome,
+    tema,
+    estilo,
+    quantidade
+):
+    
+    match estilo:
 
-    elif estilo == "clean":
-        prefixos = ["neo", "mini", "simple", "pure", "just"]
-        sufixos = ["lab", "flow", "line", "studio", "space"]
-        separadores = ["_", ".", "-", ""]
+        case "hacker":
+            prefixos= [
+                "Dark",
+                "Sys",
+                "root",
+                "Cyber",
+                "Shadow",
+                "null"
+            ]
+
+            sufixos = [
+                "fx",
+                "core",
+                "404",
+                "byte",
+                "exe",
+                "link"
+            ]
+
+        case "clean":
+
+            prefixos = [
+                "neo",
+                "mini",
+                "simple",
+                "pure",
+                "just"
+            ]
+
+            sufixos = [
+                "lab",
+                "flow",
+                "line",
+                "studio",
+                "space"
+            ]
+
+        case "profissional":
+            
+            prefixos = [
+                "dev",
+                "code",
+                "tech",
+                "mr",
+                "pro"
+            ]
+
+            sufixos = [
+                "solutions",
+                "consulting",
+                "group",
+                "services"
+            ]
+
+        case "criativo":
+
+            prefixos = [
+                 "meta",
+                "echo",
+                "nova",
+                "pixel",
+                "volt"
+            ]
+
+            sufixos = [
+                "wave",
+                "spark",
+                "lab",
+                "zone",
+                "vibe"
+            ]
+
+        case _:
+
+            prefixos = ["neo"]
+            sufixos = ["lab"]
 
 
-    elif estilo == "profissional":
-        prefixos = ["dev", "code", "tech", "mr", "pro"]
-        sufixos = ["solutions", "consulting", "group", "services", "digital", "works"]
-        separadores = ["_", ".", "-", ""]
+    separadores = [
+        "_",
+        ".",
+        "-",
+        ""
+    ]
 
+    sugestoes = [
 
-    elif estilo == "criativo":
-        prefixos = ["meta", "encho", "nova", "pixel", "volt"]
-        sufixos = ["wave", "spark", "lab", "zone", "vibe", "fusion", "motion"]
-        separadores = ["_", ".", "-", ""]
+    ]
 
-    else:
-        prefixo = ["neo", "dev", "byte"]
-        sufixos = ["lab", "core", "flow"]
-        separadores = ["_", ".", "-", ""]
+    while len(sugestoes) < quantidade:
 
-    sugestao = []
-
-    while len(sugestao) < quantidade:
         prefixo = rd.choice(prefixos)
+
         sufixo = rd.choice(sufixos)
+
         separador = rd.choice(separadores)
 
         modelo = rd.choice([
-            nome_usuario + separador + sufixo,
-            prefixo + separador + nome_usuario,
-            nome_usuario + separador + tema_usuario,
-            tema_usuario + separador + nome_usuario,
-            prefixo + nome_usuario,
-            nome_usuario + sufixo,
-            tema_usuario + sufixo,
-            prefixo + tema_usuario,
+             nome + separador + sufixo,
+
+            prefixo + separador + nome,
+
+            nome + separador + tema,
+
+            tema + separador + nome,
+
+            prefixo + nome,
+
+            nome + sufixo,
+
+            tema + sufixo,
+
+            prefixo + tema,
         ])
-        if modelo not in sugestao:
-            sugestao.append(modelo)
 
-    print("\nGerando sugestões de identidade...")
+        if modelo not in sugestoes:
 
-    print(f"\nSugestões de identidade ({quantidade}):\n")
-    for i, nome in enumerate(sugestao, start=1):
+            sugestoes.append(modelo)
+
+    return sugestoes
+
+
+def exibir_sugestoes(sugestoes):
+
+    print("\n Sugestões geradas")
+
+    for i, nome in enumerate(sugestoes, start=1):
+
         print(f"{i}. {nome}")
 
-    if estilo == "hacker":
-        print("\n Identidade gerada com perfil mais obscuro e tecnológico.")
 
-    elif estilo == "clean":
-        print("\n Identidade gerada com perfil mais minimalista e moderno.")
-
-    elif estilo == "profissional":
-        print("\n Identidade gerada com perfil mais séria e confiavel.")
-
-    elif estilo == "criativo":
-        print("\n Identidade gerada com energia original e experimental.")
-
-    print("\nIdentidade gerada com sucesso!")
-
-    repetir = input("\nDeseja gerar mais sugestões? (s/n): ").lower().strip()
-
-    
-    if repetir != 's':
-        print("\n Encerrando o Identify Forge. Até a próxima!...")
-        break
+if __name__ == "__main__":
+    main()
